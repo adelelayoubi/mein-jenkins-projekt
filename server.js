f0fd73ffe0242bf624277a44b1bd1e813e51911a
adelelayoubi <adelelayoubi@gmail.com>
2026-09-10 18:02:05 +0200

    Doppelten res.end Fehler behoben und Text aktualisiert

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hallo von meiner Jenkins CI/CD Pipeline!\n');
-  res.end('Hallo! Meine Pipeline hat sich automatisch aktualisiert!\n');
+  res.end('Hallo von meiner Jenkins CI/CD Pipeline!\nHallo! Meine Pipeline hat sich automatisch aktualisiert!\n')
 });
 
 server.listen(PORT, () => {
