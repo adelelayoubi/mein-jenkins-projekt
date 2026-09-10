92f7933a5a826725bd0d5475901e60cf8922599f
adelelayoubi <adelelayoubi@gmail.com>
2026-09-10 17:54:03 +0200

    Text in der App geändert

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hallo von meiner Jenkins CI/CD Pipeline!\n');
+  res.end('Hallo! Meine Pipeline hat sich automatisch aktualisiert!\n');
 });
 
 server.listen(PORT, () => {
